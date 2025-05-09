6f9101ea6e613b7b85f3c0602db46cf0e10536e6
nurulrahat <nurulrahat02@gmail.com>
2025-05-09 20:03:20 +0600

    assignment on typeScript

---
+++ typeScriptAssignment.ts
@@ -0,0 +1,20 @@
+function formatString(input: string, toUpper?: boolean): string {
+  if (toUpper ?? true) {
+    return input.toUpperCase();
+  } else return input.toLowerCase();
+}
+
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  return items.filter((element) => element.rating >= 4);
+}
+
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+];
+
+filterByRating(books);
+// Output: [ { title: "Book A", rating: 4.5 }, { title: "Book C", rating: 5.0 } ]
