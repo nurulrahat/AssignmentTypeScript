34eaeb9e1ab5595fb229a301e9f3f28f77b11aaf
nurulrahat <nurulrahat02@gmail.com>
2025-05-09 20:07:55 +0600

    readme file added

--- typeScriptAssignment.ts
+++ typeScriptAssignment.ts
@@ -10,11 +10,12 @@ function filterByRating(
   return items.filter((element) => element.rating >= 4);
 }
 
-const books = [
-  { title: "Book A", rating: 4.5 },
-  { title: "Book B", rating: 3.2 },
-  { title: "Book C", rating: 5.0 },
-];
+// const books = [
+//   { title: "Book A", rating: 4.5 },
+//   { title: "Book B", rating: 3.2 },
+//   { title: "Book C", rating: 5.0 },
+// ];
 
-filterByRating(books);
+// filterByRating(books);
+
 // Output: [ { title: "Book A", rating: 4.5 }, { title: "Book C", rating: 5.0 } ]
