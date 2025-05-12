cc207e3172fd21a135c46d236080705578298a95
nurulrahat <nurulrahat02@gmail.com>
2025-05-12 11:53:18 +0600

    some solutions added

--- typeScriptAssignment.ts
+++ typeScriptAssignment.ts
@@ -19,3 +19,70 @@ function filterByRating(
 // filterByRating(books);
 
 // Output: [ { title: "Book A", rating: 4.5 }, { title: "Book C", rating: 5.0 } ]
+
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+  return arrays.flat();
+}
+
+// concatenateArrays(["a", "b"], ["c"]);       // Output: ["a", "b", "c"]
+// concatenateArrays([1, 2], [3, 4], [5]);     // Output: [1, 2, 3, 4, 5]
+
+//5
+
+function processValue(value: string | number): number {
+  if (typeof value === "string") {
+    return value.length;
+  } else {
+    return value * 2;
+  }
+}
+//
+
+interface Product {
+  name: string;
+  price: number;
+}
+
+function getMostExpensiveProduct(products: Product[]): Product | null {
+  // if(products==null) return null;
+  if (products.length === 0) return null;
+
+  let highestPrice = products[0];
+
+  for (let index = 0; index < products.length; index++) {
+    if (products[index].price > highestPrice.price) {
+      highestPrice = products[index];
+    }
+  }
+  return highestPrice;
+}
+//
+enum Day {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+function getDayType(day: Day): string {
+  if (day === Day.Saturday || day === Day.Sunday) {
+    return "Weekend";
+  }
+  return "Weekday";
+}
+
+//
+async function squareAsync(n: number): Promise<number> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (n < 0) {
+        reject(new Error("Negative number not allowed"));
+      } else {
+        resolve(n * n);
+      }
+    }, 1000); // 1 second delay
+  });
+}
